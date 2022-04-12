dcc1eecc38712f1d5832c511045b87b307d65d07
Henry Nawrocki <hdev@sztanski.com>
2022-04-12 15:27:41 -0500

    Clean the code

--- src/modules/api-dom.js
+++ src/modules/api-dom.js
@@ -3,17 +3,16 @@ import { getShows } from './shows-api-helpers';
 
 const mainContainer = document.querySelector('main');
 
-console.log('start');
 export const setShows = async () => {
   const allShows = await getShows(showEndpoints.shows, [1, 5, 7, 3, 6, 9]);
   shows.push(...allShows);
-  console.log(shows);
 };
 
 export const displayTVShows = async () => {
   await setShows();
+  console.log(shows);
   // create artcile element
-  shows.forEach((show) => {
+  shows.forEach((show, i) => {
     const articleElem = document.createElement('article');
     articleElem.classList.add('show');
     articleElem.innerHTML = `
@@ -29,22 +28,8 @@ export const displayTVShows = async () => {
             >
           </p>
         </figcaption>
-        <button class="btn" data-id="${show.id}" type="button">Comments</button>
+        <button class="btn" data-id="${i}" type="button">Comments</button>
     `;
     mainContainer.appendChild(articleElem);
   });
 };
-
-/* <figure>
-       <img src="${show.image.medium}" alt="picture" />
-     </figure>
-     <figcaption>
-       <p>${show.name}</p>
-       <p>
-         <span><i class="fa-solid fa-heart"></ispan><br /><span
-           class="likes"
-           >5 likes</span
-         >
-       </p>
-     </figcaption>
-     <button class="btn" data-id="${show.id}" type="button">Commentbutton></button> */
