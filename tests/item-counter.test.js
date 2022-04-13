719d1fbce139f637891c6b6989aa8ba45cd1737f
Henry Nawrocki <hdev@sztanski.com>
2022-04-13 11:35:06 -0500

    Add babel dependencies for jest | create babel config file

--- tests/item-counter.test.js
+++ tests/item-counter.test.js
@@ -1,1 +1,8 @@
- 
+// import { shows, showEndpoints } from '../src/modules/globals';
+// const { displayItemsCount } = require('../src/modules/api-dom');
+
+describe('displayItemsCount', () => {
+  it('Displays the amount of objects fetched from the API', () => {
+    expect(5).toBe(5);
+  });
+});
