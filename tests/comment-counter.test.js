12fb437ce4d7155a69782afdf8fc7ab3b07c2ee2
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-14 21:49:52 +0300

    Implement: test for comment counter increase as new comment added

--- tests/comment-counter.test.js
+++ tests/comment-counter.test.js
@@ -3,3 +3,20 @@ import { getCommentCount } from '../src/modules/helpers-api-dom'
 import mockComment from './mock-comment';
 jest.mock('../src/modules/involvement-api-helpers');
 Helpers.getMovieComment = jest.fn((id) => Promise.resolve(mockComment));
+describe('get comments count', () => {
+  const newComment = {
+    "username": "Henry Nawrocki",
+    "creation_date": "2022-04-14",
+    "comment": "Beautiful Series"
+  }
+  it('assert comment count increase on adding comment', async () => {
+    let commentCount = await getCommentCount(1);
+    expect(commentCount).toBe(0);
+    mockComment.push(newComment);
+    commentCount = await getCommentCount(1);
+    expect(commentCount).toBe(1);
+    mockComment.push(newComment);
+    commentCount = await getCommentCount(1);
+    expect(commentCount).toBe(2);
+  })
+});
